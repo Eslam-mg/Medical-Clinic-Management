6200eb40b487be1e9ceba49e2ad625c30299598b
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-09-13 23:42:03 +0200

    add variables to Xrays GUI

--- xrays.py
+++ xrays.py
@@ -10,15 +10,28 @@ class XraysClass:
         self.root.resizable(False,False)
         self.root.config(bg='white')
 
+        # ---------- variables -----------
+        self.var_searchby = StringVar()
+        self.var_searchtxt = StringVar()
+        self.var_id = StringVar()
+        self.var_name = StringVar()
+        self.var_gender = StringVar()
+        self.var_age = StringVar()
+        self.var_date = StringVar()
+        self.var_state = StringVar()
+        self.var_price = StringVar()
+        self.var_contact = StringVar()
+        self.var_address = StringVar()
+
         # ---------- search frame ------------
         search_frame = LabelFrame(self.root, text='البحث', font=('goudy old style', 12, 'bold'), bd=2, relief=RIDGE, bg='white')
         search_frame.place(x=370, y=20, width=600, height=70)
 
-        cmb_search = ttk.Combobox(search_frame, values=("Select","Name","Contact"), justify=CENTER, state='readonly', font=('goudy old style', 15))
+        cmb_search = ttk.Combobox(search_frame, textvariable=self.var_searchby, values=("Select","Name","Contact"), justify=CENTER, state='readonly', font=('goudy old style', 15))
         cmb_search.place(x=10, y=10, width=180)
         cmb_search.current(0)
 
-        self.text_search = Entry(search_frame, font=("tajwal", 15), bg='lightyellow', justify=CENTER).place(x=200, y=10, width=210)
+        self.text_search = Entry(search_frame, textvariable=self.var_searchtxt, font=("tajwal", 15), bg='lightyellow', justify=CENTER).place(x=200, y=10, width=210)
         self.btn_search = Button(search_frame, text='بحث', font=('goudy oid style', 15), bg='#005c78', fg='white', cursor='hand2').place(x=420, y=9, width=150, height=30)
 
         # ------------ title ------------
@@ -32,30 +45,30 @@ class XraysClass:
 
         # -------- labels + entrys -------
         lbl_name = Label(self.root, text='الاسم', font=('tajwal', 15), bg='white').place(x=940, y=150)
-        en_name = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=150, width=360)
+        en_name = Entry(self.root, textvariable=self.var_name, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=150, width=360)
 
         lbl_gender = Label(self.root, text='الجنس', font=('tajwal', 15), bg='white').place(x=505, y=150)
-        cmb_gender = ttk.Combobox(self.root, values=("Select", "ذكر", "انثى"), state='readonly', justify=CENTER, font=('tajwal', 15))
+        cmb_gender = ttk.Combobox(self.root, textvariable=self.var_gender, values=("Select", "ذكر", "انثى"), state='readonly', justify=CENTER, font=('tajwal', 15))
         cmb_gender.place(x=350, y=150, width=145)
         cmb_gender.current(0)
 
         lbl_age = Label(self.root, text='العمر', font=('tajwal', 15), bg='white').place(x=940, y=195)
-        en_age = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=770, y=200, width=150)
+        en_age = Entry(self.root, textvariable=self.var_age, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=770, y=200, width=150)
         
         lbl_state = Label(self.root, text='الحالة', font=('tajwal', 15), bg='white').place(x=720, y=200)
-        en_state = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=200, width=150)
+        en_state = Entry(self.root, textvariable=self.var_state, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=200, width=150)
 
         lbl_price = Label(self.root, text='المبلغ', font=('tajwal', 15), bg='white').place(x=505, y=200)
-        en_price = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=350, y=200, width=145)
+        en_price = Entry(self.root, textvariable=self.var_price, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=350, y=200, width=145)
         
         lbl_date = Label(self.root, text='التاريخ', font=('tajwal', 15), bg='white').place(x=930, y=250)
-        en_date = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=770, y=250, width=150)
+        en_date = Entry(self.root, textvariable=self.var_date, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=770, y=250, width=150)
         
         lbl_contact = Label(self.root, text='الهاتف', font=('tajwal', 15), bg='white').place(x=715, y=250)
-        en_contact = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=250, width=150)
+        en_contact = Entry(self.root, textvariable=self.var_contact, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=560, y=250, width=150)
 
         lbl_address = Label(self.root, text='العنوان', font=('tajwal', 15), bg='white').place(x=500, y=250)
-        en_address = Entry(self.root, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=350, y=250, width=145)
+        en_address = Entry(self.root, textvariable=self.var_address, font=('tajwal', 15), bg='lightyellow', justify=CENTER).place(x=350, y=250, width=145)
 
         #  -------- buttons --------
         btn_add = Button(self.root, text='اضافة', font=('goudy old style', 15), bg='#005c78', fg='white', cursor='hand2').place(x=175, y=215, width=155, height=28)
