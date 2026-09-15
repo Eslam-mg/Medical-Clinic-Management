69731f20770639874c219e5ad1e14bd3b53cfc23
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2026-09-15 22:44:16 +0200

    add clinic database

---
+++ create_database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+def create_db ():
+    con = sqlite3.connect('clinic.db')
+    cur = con.cursor()
+    con.execute("CREATE TABLE IF NOT EXISTS xrays(address text, contact text, price text, state text, data text, age text, gender text, name text, xid INTEGER PRIMARY KEY AUTOINCREMENT)")
+    con.commit()
+
+create_db()
